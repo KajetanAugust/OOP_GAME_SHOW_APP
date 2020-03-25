6a603f3b3b28b337d510227326ceff791a5bcc8b
KajetanAugust <56959772+KajetanAugust@users.noreply.github.com>
2020-03-25 19:02:13 +0100

    getRandomPhrase ready

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,43 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+
+class Game {
+    constructor() {
+        this.missed = 0;
+        this.phrases = [
+            new Phrase('right out of the gate'),
+            new Phrase('raining cats and dogs'),
+            new Phrase('shot in the dark'),
+            new Phrase('under the weather'),
+            new Phrase('go for broke'),
+            new Phrase('quick and dirty'),
+            new Phrase('greased lightning'),
+            new Phrase('in a pickle'),
+            new Phrase('keep your shirt on'),
+            new Phrase('back to square one'),
+            new Phrase('hands down'),
+            new Phrase('wild goose chase'),
+            new Phrase('talk the talk'),
+            new Phrase('right off the bat'),
+            new Phrase('on the ropes'),
+            new Phrase('cut the mustard'),
+            new Phrase('know the ropes'),
+            new Phrase('easy as pie'),
+            new Phrase('lovey dovey'),
+            new Phrase('elephant in the room'),
+            new Phrase('son of a gun'),
+            new Phrase('a piece of cake'),
+            new Phrase('under your nose'),
+            new Phrase('eat my hat')
+        ];
+        this.activePhrase = null;
+    }
+
+    getRandomPhrase(){
+        const phrases = this.phrases;
+        return this.phrases[Math.floor(Math.random() * this.phrases.length )]
+    }
+}
+
